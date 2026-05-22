91fbbd3170e0562f1bfcdff393a26b846708381e
SmileXin886 <smilexin12138@gmail.com>
2026-05-22 18:08:32 +0800

    Delete 54 directory

--- 54/core/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * core/ 模块包入口
- * Electron 主进程专用，提供账号管理和安全浏览器启动能力
- */
-module.exports = {
-    getAccountManager: require('./account_manager').getAccountManager,
-    startSecureBrowser: require('./browser_launcher').startSecureBrowser,
-    getActiveAccountIds: require('./browser_launcher').getActiveAccountIds,
-};
